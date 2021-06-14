41d7ae3409f932a5ad751ee15b36c41ff9a2a49d
Trenix <trenixdecease@live.com>
2021-06-14 12:33:25 -0400

    Log generator has correct probability to have multiple hackers.

--- code.net/systems/generatelog.py
+++ code.net/systems/generatelog.py
@@ -4,9 +4,23 @@ import globals
 def createlog(playersonlog):
 
     if globals.players == 9:
-        tempamthacker = random.randint(1, 3)
+        if random.random() < (1 / ((globals.players + globals.aiamt) - 1)):
+            if random.random() < (1 / ((globals.players + globals.aiamt) - 2)):
+                amtbadlog = 3
+            else:
+                amtbadlog = 2
+        else:
+            amtbadlog = 1
+
+            tempamthacker = random.randint(1, amtbadlog)
     else:
-        tempamthacker = random.randint(1, 2)
+        if random.random() < (1 / ((globals.players + globals.aiamt) - 1)):
+            amtbadlog = 2
+        else:
+            amtbadlog = 1
+
+        tempamthacker = random.randint(1, amtbadlog)
+    print(amtbadlog)
 
     templog = random.sample(list(globals.coderlist), (playersonlog - tempamthacker)) + random.sample(list(globals.hackerlist), tempamthacker)
 
