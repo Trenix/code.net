49aabf8be97e9027ee7bde80ebd5be3442c1e1ad
Trenix <trenixdecease@live.com>
2021-06-18 14:01:49 -0400

    Improved filenames, optimized, and fixed AI getting a log.

--- code.net/systems/generateplayerlog.py
+++ code.net/systems/generateplayerlog.py
@@ -2,7 +2,11 @@ import random
 import globals
 
 def createplayerlog():
-    tempplayerlog = random.sample(list(globals.coderlist), 1) + random.sample(list(globals.hackerlist), 1)
+
+    if globals.players == 4 or globals.players == 5:
+        tempplayerlog = random.sample(list(globals.notai), 1) + random.sample(list(globals.hackerlist), 1)
+    else:
+        tempplayerlog = random.sample(list(globals.coderlist), 1) + random.sample(list(globals.hackerlist), 1)
 
     tempplayercolorlog = []
 
