696d469a3eab18225ce34bbb9bb7ca8198dc26d8
Trenix <trenixdecease@live.com>
2021-06-27 18:49:09 -0400

    Added complete functionality to color selection screen. Refresh, go back, and clearing last selection are all fully functional.

--- screens/colorselect.py
+++ screens/colorselect.py
@@ -4,15 +4,32 @@ from kivymd.uix.screen import MDScreen
 class ColorSelectScreen(MDScreen):
 
     def ColorSelect(self, instance):
-
+# Sets colors to players in a sequence
         if globals.colortracker > 0 and instance.icon == "brush":
             globals.playerlist[f"player {globals.playercounter}"]["color"] = instance.text
             instance.icon = "numeric-" + str(globals.playercounter) + "-circle-outline"
             globals.playercounter += 1
             globals.colortracker -= 1
-        print(globals.colortracker)
-        print(globals.playercounter)
-        print(globals.playerlist)
+
+# Clicking last color selected, will clear it
+        elif instance.icon != "brush" and instance.icon == ("numeric-" + str(globals.playercounter - 1) + "-circle-outline"):
+            instance.icon = "brush"
+            globals.playercounter -= 1
+            globals.colortracker += 1
+
+    def RefreshTracker(self):
+
+        tempvariable = []
+
+        for x in range(9):
+            tempvariable.append(f"but{x + 1}")
+
+        for x in tempvariable:
+            self.manager.get_screen("colorselect").ids[x].icon = "brush"
+
+        globals.colortracker = globals.players
+        globals.playercounter = 1
+
 
     def ClearTracker(self):
         globals.playercounter = 1
