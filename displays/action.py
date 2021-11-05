7c4a4cc9ec3fce97af637e6f0a8a4152edc054e1
Trenix <trenixdecease@live.com>
2021-11-04 20:40:15 -0400

    Have everything I need to finish the actions.

--- displays/action.py
+++ displays/action.py
@@ -1,10 +1,13 @@
 from kivymd.uix.screen import MDScreen
 from displays.dialogcode import ActionDialog
+from kivymd.uix.button import MDRoundFlatIconButton
+from functools import partial
+from kivy.utils import get_color_from_hex
+from kivy.uix.widget import Widget
 import globals
 import random
 
 class ActionScreen(MDScreen):
-
     def setactionplayers(self):
 
         # Collect players randomly that will have actions and then sort them
@@ -55,37 +58,146 @@ class ActionScreen(MDScreen):
 
         if globals.nextplayer < 4:
 
-            if globals.playeractions[globals.playeractionlist[globals.nextplayer]]["Action"] == "Code":
-                tempactiondialog.ids.actiondesc.text = "You code for the day. No meaningful action will be performed."
+            # if globals.playeractions[globals.playeractionlist[globals.nextplayer]]["Action"] == "Code":
+            tempactiondialog.ids.actiondesc.text = "You code for the day. No meaningful action will be performed."
 
-            elif globals.playeractions[globals.playeractionlist[globals.nextplayer]]["Action"] == "Analyze Log":
-                tempactiondialog.ids.actiondesc.text = "Select a log to reveal exactly how many hackers are found within it, during the time you checked it."
+            tempbutton = MDRoundFlatIconButton(
+                font_style="Button",
+                theme_text_color="Custom",
+                text="Code",
+                icon="file",
+                line_color=get_color_from_hex("#FFFFFF"),
+                icon_color=get_color_from_hex("#FFFFFF"),
+                text_color=get_color_from_hex("#FFFFFF"),
+                # Below function includes an additional argument, being the button itself.
+                on_release=partial(self.codeaction, tempactiondialog)
+            )
 
-            elif globals.playeractions[globals.playeractionlist[globals.nextplayer]]["Action"] == "Encrypt Log":
-                tempactiondialog.ids.actiondesc.text = "Select a log which prevents hackers from tampering with it."
+            # tempbutton.bind(on_release=partial(self.codeaction, tempactiondialog.ids))
 
-            elif globals.playeractions[globals.playeractionlist[globals.nextplayer]]["Action"] == "Analyze Player":
-                tempactiondialog.ids.actiondesc.text = "Select a player to check their alignment."
+            tempactiondialog.ids.addbuttons.add_widget(Widget())
+            tempactiondialog.ids.addbuttons.add_widget(tempbutton)
+            tempactiondialog.ids.addbuttons.add_widget(Widget())
 
-            elif globals.playeractions[globals.playeractionlist[globals.nextplayer]]["Action"] == "Backup Log":
-                tempactiondialog.ids.actiondesc.text = "Select a log to prevent it from being corrupted."
-
-            elif globals.playeractions[globals.playeractionlist[globals.nextplayer]]["Action"] == "Hack Digital Footprint":
-                tempactiondialog.ids.actiondesc.text = "Select a log to replace the digital footprint of a hacker with a coder."
-
-            elif globals.playeractions[globals.playeractionlist[globals.nextplayer]]["Action"] == "Corrupt Log":
-                tempactiondialog.ids.actiondesc.text = "Select a log to corrupt it."
-
-            elif globals.playeractions[globals.playeractionlist[globals.nextplayer]]["Action"] == "Infect Software":
-                tempactiondialog.ids.actiondesc.text = "If a coder preforms the analyze player or analyze log action, it will provide the wrong results."
-
-            elif globals.playeractions[globals.playeractionlist[globals.nextplayer]]["Action"] == "DDOS Server":
-                tempactiondialog.ids.actiondesc.text = "A coder will be prevented from preforming their action."
+            # elif globals.playeractions[globals.playeractionlist[globals.nextplayer]]["Action"] == "Analyze Log":
+            #     tempactiondialog.ids.actiondesc.text = "Select a log to reveal exactly how many hackers are found within it, during the time you checked it."
+            #
+            #     tempbutton = MDRoundFlatIconButton(
+            #         font_style="Button",
+            #         theme_text_color="Custom",
+            #         text="Code",
+            #         text_color=get_color_from_hex("#FFFFFF"),
+            #         pos_hint={"center_x": 0.5},
+            #     )
+            #
+            #     tempbutton.bind(on_release=partial(self.codeaction, tempactiondialog.ids))
+            #     tempactiondialog.ids.addbuttons.add_widget(tempbutton)
+            #
+            # elif globals.playeractions[globals.playeractionlist[globals.nextplayer]]["Action"] == "Encrypt Log":
+            #     tempactiondialog.ids.actiondesc.text = "Select a log which prevents hackers from tampering with it."
+            #     tempbutton = MDRoundFlatIconButton(
+            #         font_style="Button",
+            #         theme_text_color="Custom",
+            #         text="Code",
+            #         text_color=get_color_from_hex("#FFFFFF"),
+            #         pos_hint={"center_x": 0.5},
+            #     )
+            #
+            #     tempbutton.bind(on_release=partial(self.codeaction, tempactiondialog.ids))
+            #     tempactiondialog.ids.addbuttons.add_widget(tempbutton)
+            #
+            # elif globals.playeractions[globals.playeractionlist[globals.nextplayer]]["Action"] == "Analyze Player":
+            #     tempactiondialog.ids.actiondesc.text = "Select a player to check their alignment."
+            #
+            #     tempbutton = MDRoundFlatIconButton(
+            #         font_style="Button",
+            #         theme_text_color="Custom",
+            #         text_color=get_color_from_hex("#FFFFFF"),
+            #         text="Code",
+            #         pos_hint={"center_x": 0.5},
+            #     )
+            #
+            #     tempbutton.bind(on_release=partial(self.codeaction, tempactiondialog.ids))
+            #     tempactiondialog.ids.addbuttons.add_widget(tempbutton)
+            #
+            # elif globals.playeractions[globals.playeractionlist[globals.nextplayer]]["Action"] == "Backup Log":
+            #     tempactiondialog.ids.actiondesc.text = "Select a log to prevent it from being corrupted."
+            #
+            #     tempbutton = MDRoundFlatIconButton(
+            #         font_style="Button",
+            #         theme_text_color="Custom",
+            #         text="Code",
+            #         text_color=get_color_from_hex("#FFFFFF"),
+            #         pos_hint={"center_x": 0.5},
+            #     )
+            #
+            #     tempbutton.bind(on_release=partial(self.codeaction, tempactiondialog.ids))
+            #     tempactiondialog.ids.addbuttons.add_widget(tempbutton)
+            #
+            # elif globals.playeractions[globals.playeractionlist[globals.nextplayer]]["Action"] == "Hack Digital Footprint":
+            #     tempactiondialog.ids.actiondesc.text = "Select a log to replace the digital footprint of a hacker with a coder."
+            #
+            #     tempbutton = MDRoundFlatIconButton(
+            #         font_style="Button",
+            #         theme_text_color="Custom",
+            #         text="Code",
+            #         text_color=get_color_from_hex("#FFFFFF"),
+            #         pos_hint={"center_x": 0.5},
+            #     )
+            #
+            #     tempbutton.bind(on_release=partial(self.codeaction, tempactiondialog.ids))
+            #     tempactiondialog.ids.addbuttons.add_widget(tempbutton)
+            #
+            # elif globals.playeractions[globals.playeractionlist[globals.nextplayer]]["Action"] == "Corrupt Log":
+            #     tempactiondialog.ids.actiondesc.text = "Select a log to corrupt it."
+            #
+            #     tempbutton = MDRoundFlatIconButton(
+            #         font_style="Button",
+            #         theme_text_color="Custom",
+            #         text_color=get_color_from_hex("#FFFFFF"),
+            #         text="Code",
+            #         pos_hint={"center_x": 0.5},
+            #     )
+            #
+            #     tempbutton.bind(on_release=partial(self.codeaction, tempactiondialog.ids))
+            #     tempactiondialog.ids.addbuttons.add_widget(tempbutton)
+            #
+            # elif globals.playeractions[globals.playeractionlist[globals.nextplayer]]["Action"] == "Infect Software":
+            #     tempactiondialog.ids.actiondesc.text = "If a coder preforms the analyze player or analyze log action, it will provide the wrong results."
+            #
+            #     tempbutton = MDRoundFlatIconButton(
+            #         font_style="Button",
+            #         theme_text_color="Custom",
+            #         text="Code",
+            #         text_color=get_color_from_hex("#FFFFFF"),
+            #         pos_hint={"center_x": 0.5},
+            #     )
+            #
+            #     tempbutton.bind(on_release=partial(self.codeaction, tempactiondialog.ids))
+            #     tempactiondialog.ids.addbuttons.add_widget(tempbutton)
+            #
+            # elif globals.playeractions[globals.playeractionlist[globals.nextplayer]]["Action"] == "DDOS Server":
+            #     tempactiondialog.ids.actiondesc.text = "A coder will be prevented from preforming their action."
+            #
+            #     tempbutton = MDRoundFlatIconButton(
+            #         font_style="Button",
+            #         theme_text_color="Custom",
+            #         text="Code",
+            #         text_color=get_color_from_hex("#FFFFFF"),
+            #         pos_hint={"center_x": 0.5},
+            #     )
+            #
+            #     tempbutton.bind(on_release=partial(self.codeaction, tempactiondialog.ids))
+            #     tempactiondialog.ids.addbuttons.add_widget(tempbutton)
 
         else:
             pass
 
         tempactiondialog.open()
+
+    def codeaction(self, tempactiondialog, button):
+        button.disabled = True
+        tempactiondialog.ids.okaction.disabled = False
 
     def actiontracker(self):
         globals.nextplayer += 1
