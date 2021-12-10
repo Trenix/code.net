22385d017a8daa9a6081577ef03e8c75b5154798
Trenix <trenixdecease@live.com>
2021-12-10 10:51:51 -0500

    Fixed green color to not be so dark and look naturally good in dialogs.

--- globals.py
+++ globals.py
@@ -13,7 +13,7 @@ playerlogrev = []
 revealtracker = 1
 #900 Color
 colordefs = {"Red": "#c62828", "Pink": "#ad1457", "Purple": "#6a1b9a", "Blue": "#1565c0",
-             "Green": '#1b5e20', "Yellow": '#f9a825', "Orange": '#ef6c00', "Brown": '#4e342e',
+             "Green": '#2e7d32', "Yellow": '#f9a825', "Orange": '#ef6c00', "Brown": '#4e342e',
              "Grey": '#424242', "AI": '#FFFFFF'}
 time = 0
 timer = 0
